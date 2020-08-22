c6732a5dc7a7ca7883ffd3d02d3cef9b1717c79b
mohan5246 <mraj697@gmail.com>
2020-08-22 15:55:17 +0530

    Commit Done by Mohan Change-Id: Initial Gerrit change
    Change-Id: Iaf463ef951a5590d5ab658d24057a8e75124aec1

---
+++ SampleDeployProject/src/com/workfromhome/test/SampleServlet.java
@@ -0,0 +1,54 @@
+package com.workfromhome.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SampleServlet
+ */
+@WebServlet("/SampleServlet")
+public class SampleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+     private String mymsg;
+     
+     public void init(){
+    	 mymsg = "Sample HTML Message -Check";
+     }
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SampleServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		 // Setting up the content type of web page      
+        response.setContentType("text/html");
+        // Writing the message on the web page      
+        PrintWriter out = response.getWriter();      
+        out.println("<h1>" + mymsg + "</h1>");      
+        out.println("<p>" + "Hello Friends Test" + "</p>");   
+        out.println("<p>" + "Commit 2" + "</p>");   
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
